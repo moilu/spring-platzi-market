c3a24008ae360438e1d6f57b235b6946cf339301
moilu <moisesluna0390@gmail.com>
2022-02-16 08:33:28 -0800

    [created] spring project

---
+++ src/main/java/com/platzi/platzimarket/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.platzi.platzimarket;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HelloWorldController {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Nunca pares de aprender 🚀...";
+    }
+
+}
